8671e1e9f9e095dd10141cd2324070a56c60cded
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2018-05-14 17:32:10 -0300

    Rename Master.txt to Master.c

--- I2C/LED - COMUNICAÇÃO DUPLA, MASTER X SLAVE/Master.c
+++ I2C/LED - COMUNICAÇÃO DUPLA, MASTER X SLAVE/Master.c
@@ -168,7 +168,7 @@ void slaveInicioI2C(){
 }
 
 void slaveFimI2C(){
-  // Fim do envio de dados do ciclo, n„o da transmiss„o.
+  // Fim do envio de dados do ciclo, n√£o da transmiss√£o.
   UCB0IFG &= ~UCSTPIFG;
 }
 
