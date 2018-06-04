64dcaca53f3c450ab2a91b7218c4299d5b51de65
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2018-06-04 15:54:08 -0300

    Add files via upload

--- I2C/SIMULAÇÃO MAGNETOMETRO/Slave.c
+++ I2C/SIMULAÇÃO MAGNETOMETRO/Slave.c
@@ -1,21 +1,14 @@
-/****************
-Interação com o magnetometro
-(Script do SLAVE)
-
-Esse script tenta simular o funcionamento do magnetometro
-HMC5883L, podendo identificar comandos assim como enviar dados 
-para o buffer de saída.
-
-****************/
 
 #include <msp430.h>
+#include <msp430_helper.h>
 
-// Inclui a biblioteca de apoio
-#include <msp430_helper.c>
 
-char dadoAnterior = 0;
+int libera = 0;
 int counter = 0;
-char txData = [];
+char Reg = 0;
+char r1 = 0, r2 = 0, r3 = 0;
+char dadoAnterior = 0;
+char configurado = 0;
 int i;
 
 int main(void)
@@ -25,7 +18,9 @@ int main(void)
   limparLeds(); // Limpa os resultados anteriores do LED
   configurarI2C(); // Configura o I2C para as portas específicas
   tornarSlaveI2C(0x48); // Torna a placa SLAVE de endereço 0x48
-  ESTADO = 1; // Transforma a placa em transmissor.
+  P1DIR |= BIT7 + BIT6;
+  P1OUT &= ~BIT6 + ~BIT7;
+  ESTADO = 0; // Transforma a placa em receptor.
   
   while(1)
   { 
@@ -43,56 +38,67 @@ __interrupt void USCI_B0_ISR(void)
   case  0: break;                           // Vector  0: No interrupts
   case  2: break;                           // Vector  2: ALIFG
   case  4: break;                           // Vector  4: NACKIFG
-  case  6: break; // Só serve para o TX
-  case  8: break;  // Só serve para o TX // Exit LPM0 if data was trans
+  case  6: slaveInicioI2C();break; // Só serve para o TX
+  case  8: slaveFimI2C();__bic_SR_register_on_exit(LPM0_bits); break;  // Só serve para o TX // Exit LPM0 if data was trans
   case 10: 
-    // Vetor de recebimento
     if(dadoAnterior != dadosRecebidosI2C()){
-      txData[counter] = dadosRecebidosI2C();
-      limparLeds();
-      
-      if(
-         (txData[0] == 0x3C) &&
-         (txData[1] == 0x00) &&
-         (txData[2] == 0x70)
-           ) ligaLEDVerde;
-      
-      if(
-         (txData[0] == 0x3C) &&
-         (txData[1] == 0x01) &&
-         (txData[2] == 0xA0)
-         ) ligaLEDVermelho;
-      
-      if(
-         (txData[0] == 0x3C) &&
-         (txData[1] == 0x00) &&
-         (txData[2] == 0x70)
-         ) ligaLEDVerde;
-      
-      dadoAnterior = dadosRecebidosI2C();
+    limparLeds();
+    counter++;
+    switch(counter){
+    case 1: r1 = dadosRecebidosI2C();break;
+    case 2: r2 = dadosRecebidosI2C();break;
+    case 3: r3 = dadosRecebidosI2C();counter = 0;break;
     }
+    dadoAnterior=dadosRecebidosI2C();
+    }
+    
+    if(configurado < 3){
+      if(
+         (r3 == 0x3C) &&
+         (r2 == 0x00) &&
+         (r1 == 0x70)
+         ) configurado++;
+      
+      
+      if(
+         (r1 == 0x3C) &&
+         (r2 == 0x01) &&
+         (r3 == 0xA0)
+         ) configurado++;
+      
+      
+      if(
+         (r1 == 0x3C) &&
+         (r2 == 0x02) &&
+         (r3 == 0x00)
+         ) configurado++;
+    }
+    else{
+      if(
+         (r1 == 0x3D) &&
+         (r2 == 0x06) 
+           ){ ESTADO = 1;piscarLedVerde(300);}
+      
+    }
+    
+      P1OUT ^= BIT6;
+      P1OUT ^= BIT6;
+      
     __bic_SR_register_on_exit(LPM0_bits); 
     break;
   
   case 12: 
-      __delay_cycles(500000); // Tempo de envio entre um dado e outro.
+    __delay_cycles(10000);
     if(ESTADO == 1){
-            // Ao enviar 4, ambos devem mudar de estado
-      piscarLedVermelho(4); 
-      enviarDadosI2C(counter); // Transmite os dados do contador para a outra placa
-      counter++; // Soma o contador.
-        
-      if(counter > 4){ 
-        counter = 0; // Reinicia o contador  
-        ESTADO = 0; // Transforma a placa em receptor
-        limparLeds(); 
-        LigaLEDVerde;
-      }
+      enviarDadosI2C(1);
+      ESTADO = 0;
+      piscarLedVermelho(300);
+    }  
+    
     P1OUT ^= BIT7;
     P1OUT ^= BIT7;
-    }
-      __bic_SR_register_on_exit(LPM0_bits); 
-
+    
+    __bic_SR_register_on_exit(LPM0_bits); 
     break;
   default: break; 
   }
