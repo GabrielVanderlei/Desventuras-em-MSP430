db56b8477a462dbbee3a9f7b00d128d4cce88b62
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2018-06-04 16:22:09 -0300

    Update Master.c

--- I2C/SIMULAÇÃO MAGNETOMETRO/Master.c
+++ I2C/SIMULAÇÃO MAGNETOMETRO/Master.c
@@ -15,10 +15,10 @@ int i;
 int main(void)
 {
   desligarWatchDog(); // Desliga WD
-  ligarLeds(); // Direciona os LEDS para saída
+  ligarLeds(); // Direciona os LEDS para saÃ­da
   limparLeds(); // Limpa os resultados anteriores do LED
-  configurarI2C(); // Configura o I2C para as portas específicas
-  tornarMasterI2C(0x48); // Torna a placa SLAVE de endereço 0x48
+  configurarI2C(); // Configura o I2C para as portas especÃ­ficas
+  tornarMasterI2C(0x48); // Torna a placa SLAVE de endereÃ§o 0x48
   ESTADO = 1; // Transforma a placa em receptor.
     P1DIR |= BIT7 + BIT6;
   P1OUT &= ~BIT6 + ~BIT7;
@@ -39,26 +39,35 @@ __interrupt void USCI_B0_ISR(void)
   case  0: break;                           // Vector  0: No interrupts
   case  2: break;                           // Vector  2: ALIFG
   case  4: break;                           // Vector  4: NACKIFG
-  case  6: slaveInicioI2C();break; // Só serve para o TX
-  case  8: slaveFimI2C();__bic_SR_register_on_exit(LPM0_bits); break;  // Só serve para o TX // Exit LPM0 if data was trans
+  case  6: slaveInicioI2C();break; // SÃ³ serve para o TX
+  case  8: slaveFimI2C();__bic_SR_register_on_exit(LPM0_bits); break;  // SÃ³ serve para o TX // Exit LPM0 if data was trans
   case 10: 
+    if(ESTADO == 0){
     if(dadoAnterior != dadosRecebidosI2C()){
-        Reg = dadosRecebidosI2C(); 
-        ESTADO = 1;
-        piscarLedVerde(300);
-        LigaLEDVerde;
-        dadoAnterior = dadosRecebidosI2C();
+    counter++;
+    dadoAnterior = 0;
     }
-  
-    P1OUT ^= BIT6;
-    P1OUT ^= BIT6;  
     
+    if(counter < 8){
+      if(dadoAnterior != dadosRecebidosI2C()){
+          Reg = dadosRecebidosI2C(); 
+          dadoAnterior = dadosRecebidosI2C();
+      }
+      
+      piscarLedVerde(300);
+    }
+    if(counter == 8){
+      ESTADO = 1;
+      P1OUT ^= BIT6;
+      P1OUT ^= BIT6;  
+    }
+    }
     __bic_SR_register_on_exit(LPM0_bits); 
     break;
   case 12: 
 
     __delay_cycles(10000); // Tempo de envio entre um dado e outro.
-    if(ESTADO == 1){ // Está enviando
+    if(ESTADO == 1){ // EstÃ¡ enviando
       if(configurado == 0){
             counter++;
             switch(counter){
@@ -72,7 +81,7 @@ __interrupt void USCI_B0_ISR(void)
             case 5: enviarDadosI2C(0x01); break;
             case 6: enviarDadosI2C(0xA0); break;
             
-            // Medição contínua
+            // MediÃ§Ã£o contÃ­nua
             case 7: enviarDadosI2C(0x3C); break;
             case 8: enviarDadosI2C(0x02); break;
             case 9: enviarDadosI2C(0x00); configurado = 1; counter = 0; break;
@@ -86,7 +95,7 @@ __interrupt void USCI_B0_ISR(void)
         case 1: enviarDadosI2C(0x3D); break;
         case 2: enviarDadosI2C(0x06); ESTADO = 0;counter = 0;break;
         }
-      piscarLedVermelho(300);
+        piscarLedVermelho(300);
       }
       
     }
