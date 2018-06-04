07883802843831b4abcad7a735ba56b20ea24692
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2018-06-04 16:22:27 -0300

    Update Slave.c

--- I2C/SIMULAÇÃO MAGNETOMETRO/Slave.c
+++ I2C/SIMULAÇÃO MAGNETOMETRO/Slave.c
@@ -14,10 +14,10 @@ int i;
 int main(void)
 {
   desligarWatchDog(); // Desliga WD
-  ligarLeds(); // Direciona os LEDS para saída
+  ligarLeds(); // Direciona os LEDS para saÃ­da
   limparLeds(); // Limpa os resultados anteriores do LED
-  configurarI2C(); // Configura o I2C para as portas específicas
-  tornarSlaveI2C(0x48); // Torna a placa SLAVE de endereço 0x48
+  configurarI2C(); // Configura o I2C para as portas especÃ­ficas
+  tornarSlaveI2C(0x48); // Torna a placa SLAVE de endereÃ§o 0x48
   P1DIR |= BIT7 + BIT6;
   P1OUT &= ~BIT6 + ~BIT7;
   ESTADO = 0; // Transforma a placa em receptor.
@@ -38,9 +38,10 @@ __interrupt void USCI_B0_ISR(void)
   case  0: break;                           // Vector  0: No interrupts
   case  2: break;                           // Vector  2: ALIFG
   case  4: break;                           // Vector  4: NACKIFG
-  case  6: slaveInicioI2C();break; // Só serve para o TX
-  case  8: slaveFimI2C();__bic_SR_register_on_exit(LPM0_bits); break;  // Só serve para o TX // Exit LPM0 if data was trans
+  case  6: slaveInicioI2C();break; // SÃ³ serve para o TX
+  case  8: slaveFimI2C();__bic_SR_register_on_exit(LPM0_bits); break;  // SÃ³ serve para o TX // Exit LPM0 if data was trans
   case 10: 
+    if(ESTADO == 0){
     if(dadoAnterior != dadosRecebidosI2C()){
     limparLeds();
     counter++;
@@ -77,26 +78,27 @@ __interrupt void USCI_B0_ISR(void)
       if(
          (r1 == 0x3D) &&
          (r2 == 0x06) 
-           ){ ESTADO = 1;piscarLedVerde(300);}
+           ){ ESTADO = 1;piscarLedVerde(300);r1 = 0; r2 = 0;}
       
     }
     
       P1OUT ^= BIT6;
       P1OUT ^= BIT6;
-      
+    }
     __bic_SR_register_on_exit(LPM0_bits); 
     break;
   
   case 12: 
     __delay_cycles(10000);
     if(ESTADO == 1){
-      enviarDadosI2C(1);
-      ESTADO = 0;
+      counter++;
+      if(counter <= 8) enviarDadosI2C(counter);
+      if(counter == 8){ ESTADO = 0; counter = 0;}
       piscarLedVermelho(300);
+    P1OUT ^= BIT7;
+    P1OUT ^= BIT7;
     }  
     
-    P1OUT ^= BIT7;
-    P1OUT ^= BIT7;
     
     __bic_SR_register_on_exit(LPM0_bits); 
     break;
